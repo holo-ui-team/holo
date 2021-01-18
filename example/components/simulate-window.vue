0d46da20c364f1d2d9ced12d08483bdd3eefe749
chenxj25123 <308666258@qq.com>
2021-01-18 15:33:37 +0800

    button组件

---
+++ example/components/simulate-window.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="simulate-wapper">
+    <div class="title">{{ title }}</div>
+    <div class="main-body" :style="{ background : bgColor }">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SimulateWindow extends Vue {
+  @Prop( { default: '' } ) readonly title!: String;
+  @Prop( { default: '#ffffff' } ) readonly bgColor!: String;
+}
+</script>
+
+<style lang="less" scoped>
+.simulate-wapper {
+  width: 375px; max-height: 1000px; min-height: 700px;
+  border: 1px solid #ccc;
+  .title {
+    height: 46px;
+    background: #ccc;
+    color: #333; font-size: 18px; text-align: center; line-height: 46px;
+  }
+  .main-body {
+    padding: 16px;
+  }
+}
+</style>
