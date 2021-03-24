98518702f42fd7e93ee8a4c86390a7265115ea21
chenshiyu <chenshiy@133.cn>
2021-03-24 11:18:54 +0800

    【additionnal】popup/icon

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -1,7 +1,2 @@
-import button from './button.vue';
-
-const oButton: any = function(Vue) {
-  Vue.component('oButton', button);
-}
-
-export default oButton;
+import oButton from './button.vue';
+export default oButton
