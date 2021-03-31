ca91dfe197856caa41b99ad9a8703fa96881617b
chenshiyu <chenshiy@133.cn>
2021-03-31 18:18:30 +0800

    【clear】

--- packages/Icon/index.ts
+++ packages/Icon/index.ts
@@ -1,7 +1,8 @@
-import component from './index.vue';
+import { VueConstructor } from 'vue/types/umd'
+import component from './index.vue'
 
-const oIcon: any = function(Vue) {
-  Vue.component('oIcon', component);
+const oIcon: any = function(Vue: VueConstructor) {
+  Vue.component('oIcon', component)
 }
 
-export default oIcon;
+export default oIcon
--- packages/PopupOption/index.ts
+++ packages/PopupOption/index.ts
@@ -1,9 +1,9 @@
 import { VueConstructor } from 'vue'
-import component from './index.vue';
+import component from './index.vue'
 
 const oPopupOption = function(Vue: VueConstructor) {
   Vue.component('oPopupOption', component)
 }
 
-export default oPopupOption;
+export default oPopupOption
 
